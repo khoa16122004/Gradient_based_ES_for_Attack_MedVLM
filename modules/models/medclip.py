dc40490dd0b45f842bd72e01d4d652ea47088678
khoa16122004 <khoatr.work@gmail.com>
2025-12-17 21:15:32 +0700

    Update medclip.py

--- modules/models/medclip.py
+++ modules/models/medclip.py
@@ -164,7 +164,7 @@ class MedCLIPModel(VisionLanguageModel):
             file_name = "medclip.pt"
             local_path = hf_hub_download(repo_id=repo_id, filename=file_name)
             model_state_dict = torch.load(local_path)['model_state_dict']
-            model_state_dict = self._strip_prefix_from_state_dict(model_state_dict, 'model.')
+            # model_state_dict = self._strip_prefix_from_state_dict(model_state_dict, 'model.')
 
             incompatible_keys = self.load_state_dict(model_state_dict, strict=True)
             print(f"Incompatible keys when load {local_path}: {incompatible_keys}")
