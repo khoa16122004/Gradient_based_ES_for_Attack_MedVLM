61376106b8f6c5ca6828af65789e4f7babfc9c1f
khoa16122004 <khoatr.work@gmail.com>
2025-12-23 15:24:00 +0700

    Update attack.py

--- modules/attack/attack.py
+++ modules/attack/attack.py
@@ -489,7 +489,7 @@ class NES_Attack(BaseAttack):
             grad = grad / (2 * self.sigma * used_q)
 
             # update
-            delta = delta - self.alpha * grad
+            delta = delta - self.alpha * grad.sign()
             delta = project_delta(delta, self.eps, self.norm)
 
             margin, l2 = self.evaluator.evaluate_blackbox(delta)
@@ -503,7 +503,7 @@ class NES_Attack(BaseAttack):
 
             if self.is_success(f_m) and success_evaluation is None:
                 success_evaluation = num_evaluation
-                break
+                # break
 
         return {
             "best_delta": delta.detach(),
