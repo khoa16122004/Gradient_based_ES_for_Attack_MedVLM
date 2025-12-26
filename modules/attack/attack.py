1aa8f5014a5363c098f3a6019e6728d1b210d323
khoa16122004 <khoatr.work@gmail.com>
2025-12-26 11:24:04 +0700

    Update attack.py

--- modules/attack/attack.py
+++ modules/attack/attack.py
@@ -511,139 +511,281 @@ class NES_Attack(BaseAttack):
         }
 
 
-def grid_decode(z, patch_size, H, W):
-    """
-    z: (B, N_patch, 3)
-    return: (B, 3, H, W)
-    """
-    B, N, C = z.shape
-    ph = pw = patch_size
-    gh = H // ph
-    gw = W // pw
+# def grid_decode(z, patch_size, H, W):
+#     """
+#     z: (B, N_patch, 3)
+#     return: (B, 3, H, W)
+#     """
+#     B, N, C = z.shape
+#     ph = pw = patch_size
+#     gh = H // ph
+#     gw = W // pw
 
-    z = z.view(B, gh, gw, C)          # B,gh,gw,3
-    z = z.permute(0, 3, 1, 2)          # B,3,gh,gw
-    z = z.repeat_interleave(ph, dim=2)
-    z = z.repeat_interleave(pw, dim=3)
-    return z
+#     z = z.view(B, gh, gw, C)          # B,gh,gw,3
+#     z = z.permute(0, 3, 1, 2)          # B,3,gh,gw
+#     z = z.repeat_interleave(ph, dim=2)
+#     z = z.repeat_interleave(pw, dim=3)
+#     return z
 
-class GridAttack(BaseAttack):
-    def __init__(self, evaluator, patch_size=16, **kwargs):
-        super().__init__(evaluator, **kwargs)
+# class GridAttack(BaseAttack):
+#     def __init__(self, evaluator, patch_size=16, **kwargs):
+#         super().__init__(evaluator, **kwargs)
 
-        _, self.C, self.H, self.W = evaluator.img_tensor.shape
-        self.patch = patch_size
-        assert self.H % patch_size == 0
-        assert self.W % patch_size == 0
+#         _, self.C, self.H, self.W = evaluator.img_tensor.shape
+#         self.patch = patch_size
+#         assert self.H % patch_size == 0
+#         assert self.W % patch_size == 0
 
-        self.gh = self.H // patch_size
-        self.gw = self.W // patch_size
-        self.Np = self.gh * self.gw   # number of patches
+#         self.gh = self.H // patch_size
+#         self.gw = self.W // patch_size
+#         self.Np = self.gh * self.gw   # number of patches
 
-    def z_to_delta(self, z):
-        """
-        z: (B, N_patch, 3)
-        """
-        s = torch.tanh(z)
-        delta = grid_decode(s, self.patch, self.H, self.W)
-        return self.eps * delta
+#     def z_to_delta(self, z):
+#         """
+#         z: (B, N_patch, 3)
+#         """
+#         s = torch.tanh(z)
+#         delta = grid_decode(s, self.patch, self.H, self.W)
+#         return self.eps * delta
 
-class GridES_1_Lambda(GridAttack):
+# class GridES_1_Lambda(GridAttack):
+#     def __init__(
+#         self,
+#         evaluator,
+#         patch_size=16,
+#         eps=8/255,
+#         norm="linf",
+#         max_evaluation=10000,
+#         lam=64,
+#         c_inc=1.5,
+#         c_dec=0.9,
+#         sigma=1.0,
+#         device="cuda"
+#     ):
+#         super().__init__(
+#             evaluator=evaluator,
+#             patch_size=patch_size,
+#             eps=eps,
+#             norm=norm,
+#             device=device
+#         )
+
+#         self.lam = lam
+#         self.c_inc = c_inc
+#         self.c_dec = c_dec
+#         self.sigma = sigma
+#         self.max_evaluation = max_evaluation
+
+#     def run(self):
+
+#         # mean in patch-space
+#         m = torch.randn(
+#             (1, self.Np, self.C),
+#             device=self.device
+#         )
+
+#         delta_m = project_delta(
+#             self.z_to_delta(m),
+#             self.eps,
+#             self.norm
+#         )
+
+#         f_m, l2_m = self.evaluator.evaluate_blackbox(delta_m)
+#         num_eval = 1
+#         history = [(num_eval, float(f_m))]
+
+#         success_eval = None
+#         sigma = self.sigma
+
+#         while num_eval < self.max_evaluation:
+
+#             noise = torch.randn(
+#                 (self.lam, self.Np, self.C),
+#                 device=self.device,
+#                 generator=g_gpu
+#             )
+
+#             Z = m + sigma * noise
+
+#             deltas = project_delta(
+#                 self.z_to_delta(Z),
+#                 self.eps,
+#                 self.norm
+#             )
+
+#             margins, l2s = self.evaluate_population(deltas)
+#             num_eval += self.lam
+
+#             idx = torch.argmin(margins)
+#             f_best = float(margins[idx])
+#             l2_best = float(l2s[idx])
+
+#             if f_best < f_m:
+#                 m = Z[idx:idx+1].clone()
+#                 f_m = f_best
+#                 l2_m = l2_best
+#                 sigma *= self.c_inc
+#             else:
+#                 sigma *= self.c_dec
+
+#             history.append((num_eval, f_m))
+
+#             if self.is_success(f_m) and success_eval is None:
+#                 success_eval = num_eval
+#                 break
+
+#             print(
+#                 f"[Eval {num_eval}] "
+#                 f"Best margin: {f_m:.6f} | L2: {l2_m:.4f}"
+#             )
+
+#         return {
+#             "best_delta": self.z_to_delta(m).detach(),
+#             "best_margin": f_m,
+#             "history": history,
+#             "success_evaluation": success_eval
+#         }
+
+
+def grid_patch_mask(patch_idx, patch_size, H, W, device):
+    gh = H // patch_size
+    gw = W // patch_size
+
+    i = patch_idx // gw
+    j = patch_idx % gw
+
+    mask = torch.zeros((1, 1, H, W), device=device)
+
+    h0 = i * patch_size
+    h1 = h0 + patch_size
+    w0 = j * patch_size
+    w1 = w0 + patch_size
+
+    mask[:, :, h0:h1, w0:w1] = 1.0
+    return mask
+
+def grid_local_es(
+    evaluator,
+    base_delta,
+    mask,
+    eps,
+    norm,
+    lam=16,
+    steps=10,
+    sigma=0.5,
+    device="cuda"
+):
+    _, C, H, W = base_delta.shape
+
+    z = torch.zeros_like(base_delta)
+    best_delta = base_delta.clone()
+
+    f_best, _ = evaluator.evaluate_blackbox(best_delta)
+
+    for _ in range(steps):
+
+        noise = torch.randn((lam, C, H, W), device=device)
+        Z = z + sigma * noise
+
+        deltas = base_delta + mask * torch.tanh(Z) * eps
+        deltas = project_delta(deltas, eps, norm)
+
+        margins, _ = evaluator.evaluate_blackbox(deltas)
+        idx = torch.argmin(margins)
+
+        if margins[idx] < f_best:
+            z = Z[idx:idx+1].clone()
+            best_delta = deltas[idx:idx+1].clone()
+            f_best = margins[idx]
+        else:
+            sigma *= 0.7
+
+    return best_delta, float(f_best)
+
+
+class GridES_Explore_1_Lambda(BaseAttack):
     def __init__(
         self,
         evaluator,
         patch_size=16,
         eps=8/255,
         norm="linf",
+        lam=16,
+        local_steps=1000,
+        sigma=0.5,
         max_evaluation=10000,
-        lam=64,
-        c_inc=1.5,
-        c_dec=0.9,
-        sigma=1.0,
         device="cuda"
     ):
-        super().__init__(
-            evaluator=evaluator,
-            patch_size=patch_size,
-            eps=eps,
-            norm=norm,
-            device=device
-        )
+        super().__init__(evaluator, eps, norm, device)
+
+        _, _, self.H, self.W = evaluator.img_tensor.shape
+        self.patch = patch_size
+        self.gh = self.H // patch_size
+        self.gw = self.W // patch_size
+        self.Np = self.gh * self.gw
 
         self.lam = lam
-        self.c_inc = c_inc
-        self.c_dec = c_dec
+        self.local_steps = local_steps
         self.sigma = sigma
         self.max_evaluation = max_evaluation
 
     def run(self):
 
-        # mean in patch-space
-        m = torch.randn(
-            (1, self.Np, self.C),
-            device=self.device
-        )
+        delta = torch.zeros_like(self.evaluator.img_tensor)
+        f_best, _ = self.evaluator.evaluate_blackbox(delta)
 
-        delta_m = project_delta(
-            self.z_to_delta(m),
-            self.eps,
-            self.norm
-        )
+        used_eval = 1
+        tried = set()
 
-        f_m, l2_m = self.evaluator.evaluate_blackbox(delta_m)
-        num_eval = 1
-        history = [(num_eval, float(f_m))]
+        while used_eval < self.max_evaluation:
 
-        success_eval = None
-        sigma = self.sigma
+            # --- Grid exploration: random patch
+            if len(tried) == self.Np:
+                tried.clear()
 
-        while num_eval < self.max_evaluation:
+            patch_idx = np.random.choice(
+                list(set(range(self.Np)) - tried)
+            )
+            tried.add(patch_idx)
 
-            noise = torch.randn(
-                (self.lam, self.Np, self.C),
-                device=self.device,
-                generator=g_gpu
+            mask = grid_patch_mask(
+                patch_idx,
+                self.patch,
+                self.H,
+                self.W,
+                self.device
             )
 
-            Z = m + sigma * noise
-
-            deltas = project_delta(
-                self.z_to_delta(Z),
-                self.eps,
-                self.norm
+            delta_new, f_new = grid_local_es(
+                evaluator=self.evaluator,
+                base_delta=delta,
+                mask=mask,
+                eps=self.eps,
+                norm=self.norm,
+                lam=self.lam,
+                steps=self.local_steps,
+                sigma=self.sigma,
+                device=self.device
             )
 
-            margins, l2s = self.evaluate_population(deltas)
-            num_eval += self.lam
+            used_eval += self.lam * self.local_steps
 
-            idx = torch.argmin(margins)
-            f_best = float(margins[idx])
-            l2_best = float(l2s[idx])
-
-            if f_best < f_m:
-                m = Z[idx:idx+1].clone()
-                f_m = f_best
-                l2_m = l2_best
-                sigma *= self.c_inc
+            if f_new < f_best:
+                delta = delta_new
+                f_best = f_new
+                tried.clear()  # exploit tiếp patch mới
+                print(f"[Grid Patch {patch_idx}] Improved -> {f_best:.6f}")
             else:
-                sigma *= self.c_dec
+                print(f"[Grid Patch {patch_idx}] No improvement")
 
-            history.append((num_eval, f_m))
-
-            if self.is_success(f_m) and success_eval is None:
-                success_eval = num_eval
+            if self.is_success(f_best):
                 break
 
-            print(
-                f"[Eval {num_eval}] "
-                f"Best margin: {f_m:.6f} | L2: {l2_m:.4f}"
-            )
-
         return {
-            "best_delta": self.z_to_delta(m).detach(),
-            "best_margin": f_m,
-            "history": history,
-            "success_evaluation": success_eval
+            "best_delta": delta.detach(),
+            "best_margin": f_best,
+            "num_evaluation": used_eval
         }
+
 
 
