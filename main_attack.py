28d40b7e062f7d08c0813e1365e93f9c0670b9ad
khoa16122004 <khoatr.work@gmail.com>
2025-12-27 10:31:30 +0700

    Update main_attack.py

--- main_attack.py
+++ main_attack.py
@@ -115,7 +115,7 @@ def main(args):
     # path dir save
     if args.attacker_name == "ES_1_Lambda":
         # ko có lkambda mặt định là 50
-        save_dir = os.path.join(args.out_dir, args.model_name, args.dataset_name, f"attack_name={args.attacker_name}_mode={args.mode}_epsilon={args.epsilon}_lamda={args.lamda}_norm={args.norm}_seed={args.seed}")
+        save_dir = os.path.join(args.out_dir, args.model_name, args.dataset_name, f"attack_name={args.attacker_name}_mode={args.mode}_epsilon={args.epsilon}_pattern={args.pattern}_lamda={args.lamda}_norm={args.norm}_seed={args.seed}")
     elif args.attacker_name == "NES":
         save_dir = os.path.join(args.out_dir, args.model_name, args.dataset_name, f"attack_name={args.attacker_name}_mode={args.mode}_epsilon={args.epsilon}_q={args.q}_alpha={args.alpha}_norm={args.norm}_seed={args.seed}")
 
