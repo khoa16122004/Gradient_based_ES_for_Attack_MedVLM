3add1272a25be1a1ca4bc1ae51f97cd2bfc70b2a
khoa16122004 <khoatr.work@gmail.com>
2025-12-31 10:04:40 +0700

    Update evaluator.py

--- modules/attack/evaluator.py
+++ modules/attack/evaluator.py
@@ -66,7 +66,7 @@ class EvaluatePerturbation:
         mask = torch.ones_like(sims, dtype=bool)
         mask[:, self.clean_pred_id] = False
         other_max_sim = sims[mask].view(sims.size(0), -1).max(dim=1, keepdim=True).values  # (B, 1)
-        margin = correct_sim - other_max_sim + 0.05
+        margin = correct_sim - other_max_sim
 
         if self.target_image_feat is not None:
             target_sim = adv_feats @ self.target_image_feat.T
