8ddf3a5dd0b87139308cdce080c18c729afc2691
khoa16122004 <khoatr.work@gmail.com>
2026-01-20 22:26:30 +0700

    init

--- run.sh
+++ run.sh
@@ -8,10 +8,10 @@ python main_attack.py \
     --norm linf \
     --max_evaluation 10000 \
     --lamda 50 \
-    --out_dir attack_results/rsna_ES_1_Lambda \
+    --out_dir test_visualization \
     --start_idx 0 \
-    --end_idx 1000 \
-    --index_path "evaluate_result\union_data.txt"
+    --end_idx 1 \
+    --index_path "evaluate_result\intersection.txt"
 
 # python main_attack.py \
 #     --dataset_name rsna \
@@ -21,7 +21,57 @@ python main_attack.py \
 #     --norm linf \
 #     --PGD_steps 100 \
 #     --alpha 0.01 \
-#     --out_dir attack_results/rsna_ES_1_Lambda \
+#     --out_dir attack_results/RSNA_PGD \
 #     --start_idx 0 \
 #     --end_idx 1000 \
 #     --index_path "evaluate_result\union_data.txt"
+
+# python main_attack.py \
+#     --dataset_name rsna \
+#     --model_name ViT-B-16 \
+#     --attacker_name PGD \
+#     --epsilon 0.03 \
+#     --norm linf \
+#     --PGD_steps 100 \
+#     --alpha 0.01 \
+#     --out_dir attack_results/RSNA_PGD \
+#     --start_idx 0 \
+#     --end_idx 1000 \
+#     --index_path "evaluate_result\union_data.txt"
+    
+# python main_attack.py \
+#     --dataset_name rsna \
+#     --model_name ViT-L-14 \
+#     --attacker_name PGD \
+#     --epsilon 0.03 \
+#     --norm linf \
+#     --PGD_steps 100 \
+#     --alpha 0.01 \
+#     --out_dir attack_results/RSNA_PGD \
+#     --start_idx 0 \
+#     --end_idx 1000 \
+# #     --index_path "evaluate_result\union_data.txt"
+
+# python main_attack.py \
+#     --dataset_name rsna \
+#     --model_name medclip \
+#     --attacker_name PGD \
+#     --epsilon 0.03 \
+#     --norm linf \
+#     --PGD_steps 100 \
+#     --alpha 0.01 \
+#     --out_dir attack_results/RSNA_PGD \
+#     --start_idx 0 \
+#     --end_idx 1000 \
+#     --index_path "evaluate_result\union_data.txt"
+
+# python transfer_attack.py \
+#     --dataset_name rsna \
+#     --model_name ViT-B-16 \
+#     --transfer_dir "attack_results\rsna_ES_1_Lambda\ViT-B-32\rsna\attack_name=ES_1_Lambda_epsilon=0.03_lamda=50_norm=linf_seed=42" \
+#     --start_idx 0 \
+#     --end_idx 1000 \
+#     --index_path "evaluate_result\union_data.txt"
+
+
+
--- script/take_union_data.py
+++ script/take_union_data.py
@@ -2,19 +2,35 @@ import os
 import json
 import random
 
-file_path = "D:\Gradient_based_ES_for_Attack_MedVLM\evaluate_result"
+file_path = r"D:\Gradient_based_ES_for_Attack_MedVLM\evaluate_result"
 
-union_indexs = [set(), set()]
+indexs = [None, None]  # để biết file đầu tiên
+
 for file_name in os.listdir(file_path):
     json_path = os.path.join(file_path, file_name)
     with open(json_path, 'r') as f:
         data = json.load(f)
-        print(len(data))
-        for sample in data:
-            union_indexs[sample['gt_id']].add(sample['id'])
 
-sample_0 = random.sample(list(union_indexs[0]), 500)
-sample_1 = random.sample(list(union_indexs[1]), 500)    
-with open("union_data.txt", 'w') as f:
-    for line in sample_0 + sample_1:
-        f.write(str(line) + '\n')
+    current_samples = [set(), set()]
+    for sample in data:
+        current_samples[sample['gt_id']].add(sample['id'])
+
+    if indexs[0] is None:
+        indexs[0] = current_samples[0]
+        indexs[1] = current_samples[1]
+    else:
+        indexs[0] &= current_samples[0]
+        indexs[1] &= current_samples[1]
+
+# convert sang list
+index_0 = list(indexs[0])
+index_1 = list(indexs[1])
+
+sample_0 = random.sample(index_0, min(500, len(index_0)))
+sample_1 = random.sample(index_1, min(500, len(index_1)))
+
+final = sample_0 + sample_1
+
+with open("intersection.txt", "w") as f:
+    for x in final:
+        f.write(str(x) + "\n")
