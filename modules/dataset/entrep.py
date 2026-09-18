b053b3f1fb0a391b9e44bc00c4a219607d2a6621
khoa16122004 <khoatr.work@gmail.com>
2026-09-18 14:28:21 +0700

    Update entrep.py

--- modules/dataset/entrep.py
+++ modules/dataset/entrep.py
@@ -134,7 +134,7 @@ class ENTREPDataset(BaseContrastiveDataset):
         # input()
         
         # Check if required files exist
-        data_csv_path = os.path.join(entrep_data_path, "entrep_data.csv")
+        data_csv_path = os.path.join(entrep_data_path, "entrep-data.csv")
         train_csv_path = os.path.join(entrep_data_path, "entrep-train-meta.csv")
         test_csv_path = os.path.join(entrep_data_path, "entrep-test-meta.csv")
         val_csv_path = os.path.join(entrep_data_path, "entrep-val-meta.csv")
